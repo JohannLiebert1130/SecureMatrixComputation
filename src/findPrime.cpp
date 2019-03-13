254cbe464d664d954bec3b55af7e0c0b59aad0b9
JohannLiebert1130 <672016243@qq.com>
2019-03-13 15:51:59 +0800

    finds bigger prime numbers

--- src/findPrime.cpp
+++ src/findPrime.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-const int MAXN = 3300000;
+const int MAXN = 66000000;
 bool p[MAXN] = {false};     //如果ｉ为素数则p[i] 为　false 
 
 void SavePrime(ofstream& file)
@@ -11,7 +11,7 @@ void SavePrime(ofstream& file)
     {
         for(int i = 2; i < MAXN; i++)
         {
-            if(p[i] == false)
+            if(p[i] == false && i > 3300000)
             {
                 file << i << "\n";
                 for(int j = 2*i; j < MAXN; j += i)
